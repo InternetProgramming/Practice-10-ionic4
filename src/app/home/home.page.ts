a88c944a12cb029761504129c44b6a397cf70a27
Andrea Rojas <andrear.2810@gmail.com>
2021-05-25 18:17:23 -0500

    First commit, basic app, with a circle progress

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -9,4 +9,27 @@ export class HomePage {
 
   constructor() {}
 
+  elapsed: any = {
+    h: '00',
+    m: '00',
+    s: '00'
+  }
+  progress:any = 0;
+  overallProgress:any = 0;
+  percent: number = 0;
+  radius: number = 100;
+  minutes: number = 1;
+  seconds: any = 10;
+  timer: any = false;
+  overallTimer: any = false;
+  fullTime: any = '00:01:30';
+
+
+  countDownTimer: any = false;
+  timeLeft: any = {
+    m: '00',
+    s: '00'
+  };
+
+
 }
